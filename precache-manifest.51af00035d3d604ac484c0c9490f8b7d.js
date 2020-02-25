8d99ef058de11042cea343dbfe6e328e069e7ef6
Jhonatan <jhonatancardoso09@gmail.com>
2020-02-25 03:21:14 -0300

    Updates

---
+++ precache-manifest.51af00035d3d604ac484c0c9490f8b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046cd112607a47d4cf98a2dbf2bbb259",
+    "url": "/PyramidAsteriskGenerator/index.html"
+  },
+  {
+    "revision": "b1d6b05b22c4b935ab8a",
+    "url": "/PyramidAsteriskGenerator/static/css/main.0cba93a3.chunk.css"
+  },
+  {
+    "revision": "92ad57a16a7c724e1042",
+    "url": "/PyramidAsteriskGenerator/static/js/2.55247944.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PyramidAsteriskGenerator/static/js/2.55247944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d6b05b22c4b935ab8a",
+    "url": "/PyramidAsteriskGenerator/static/js/main.9eecd1e5.chunk.js"
+  },
+  {
+    "revision": "21081b896ace986252c2",
+    "url": "/PyramidAsteriskGenerator/static/js/runtime-main.bcd3847a.js"
+  }
+]);
